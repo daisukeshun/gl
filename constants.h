19be364134f5667dd7015f3dd8e3628b884f97ca
daisukeshun <40919307+daisukeshun@users.noreply.github.com>
2019-07-20 02:04:20 +0300

    Initial commit

---
+++ constants.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+// rotation speed 
+#define ANGLE_SPEED 5;
+
+// this variables use for set mesh further from the camera
+#define zx 10
+#define zy 10
+
+// 21:9, 16:9, 4:3 etc.
+const float aspectRatio = WINDOW_WIDTH / WINDOW_HEIGHT;
+
---
+++ includes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <SDL.h>
+#include "simpleGeometry.h"
+#include "OBJLoader.h"
+#include "constants.h"
+
